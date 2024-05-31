27c004543bc8822d0fb5166a0ba06adc8fe64d8a
MXhch <mihajloisuk021@gmail.com>
2024-05-31 15:14:37 +0200

    model debug and makemigration

--- library/books/models.py
+++ library/books/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-  _GENRES = [
-    "SciFi",
-    "Fantasy",
-    "Thriller",
-    "Romance",
-    "Mystery",
-    "Autobiography",
-  ]
+  _GENRES = (
+    ("SciFi", "SciFi"),
+    ("Fantasy", "Fantasy"),
+    ("Thriller", "Thriller"),
+    ("Romance", "Romance"),
+    ("Mystery", "Mystery"),
+    ("Autobiography", "Autobiography"),
+  )
 
   title = models.CharField(max_length=150)
   author = models.CharField(max_length=70)
   cover = models.ImageField(storage="books/images/covers/")
   genre = models.CharField(max_length=50, choices=_GENRES)
-  book_file = models.FileField(_("PDF File"), upload_to="books/pdf/", max_length=100)
+  book_file = models.FileField("PDF File", upload_to="books/pdf/", max_length=100)
