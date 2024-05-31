c6d9a51f6192bbec6e2667315c0b8ddfcd820de0
MXhch <mihajloisuk021@gmail.com>
2024-05-31 13:59:56 +0200

    create book app and set it up

---
+++ books/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+# Create your URLs here.
+urlpatterns = [
+  
+]
--- library/core/settings.py
+++ library/core/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-&6&yy_13^w+jt532&@=*!@7p^ld*lwa4r2=qhf3u3mqe1dljky
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  '*',
+]
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL  = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+MEDIA_URL   = '/media/'
+MEDIA_ROOT  = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
