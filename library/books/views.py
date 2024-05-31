3d2472a0e19d437204ab643e922d4284fc411e77
MXhch <mihajloisuk021@gmail.com>
2024-05-31 14:09:20 +0200

    Basic frontend set up

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.base import ContextMixin
 
 # Create your views here.
+class HomepageView(TemplateView, ContextMixin):
+  template_name = "books/homepage.html"
