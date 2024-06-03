f2fe5a4988d71bac26f287010eebaac99d633e7a
MXhch <mihajloisuk021@gmail.com>
2024-06-03 17:27:59 +0200

    homepage static content

--- library/books/urls.py
+++ library/books/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from .views_api import BookViewSet
 from rest_framework.routers import DefaultRouter
+from .views import HomepageView
 
 # Create your URLs here.
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
 urlpatterns = [
-  path('api/', include(router.urls))
+  path('', HomepageView.as_view(), name="homepage"),
+  path('api/', include(router.urls)),
 ]
