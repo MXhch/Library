d65496f873e06594bf167ac16f674a2b671dd1b5
MXhch <mihajloisuk021@gmail.com>
2024-06-03 17:40:06 +0200

    add header

--- library/books/urls.py
+++ library/books/urls.py
@@ -8,5 +8,6 @@ router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
 urlpatterns = [
   path('', HomepageView.as_view(), name="homepage"),
+  path('library/', HomepageView.as_view(), name="library"),
   path('api/', include(router.urls)),
 ]
